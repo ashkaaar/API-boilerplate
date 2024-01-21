0d5b615df82af580bcf35a6e004e66cf98a37f0b
av1shkar <vishkardandge@gmail.com>
2024-01-21 18:32:07 +0530

    mongodb setup

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Add this line
+app.use(express.json());
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
