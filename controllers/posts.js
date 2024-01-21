09f333a52414e6370183220917c5d9e723c01ce3
av1shkar <vishkardandge@gmail.com>
2024-01-21 18:11:48 +0530

    add controller functions

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,21 +1,56 @@
 const Post = require('../models/post');
 
 exports.createPost = async (req, res) => {
-    // Implement the function to create a post
+    const post = new Post(req.body);
+    try {
+        await post.save();
+        res.status(201).send(post);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 };
 
 exports.getPosts = async (req, res) => {
-    // Implement the function to get all posts
+    try {
+        const posts = await Post.find({});
+        res.send(posts);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 exports.getPost = async (req, res) => {
-    // Implement the function to get a single post
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).send();
+        }
+        res.send(post);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
 
 exports.updatePost = async (req, res) => {
-    // Implement the function to update a post
+    try {
+        const post = await Post.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!post) {
+            return res.status(404).send();
+        }
+        res.send(post);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 };
 
 exports.deletePost = async (req, res) => {
-    // Implement the function to delete a post
+    try {
+        const post = await Post.findByIdAndDelete(req.params.id);
+        if (!post) {
+            return res.status(404).send();
+        }
+        res.send(post);
+    } catch (err) {
+        res.status(500).send(err);
+    }
 };
