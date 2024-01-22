f7604cf5641916b8f9e707fe4185651d178c8fdf
av1shkar <vishkardandge@gmail.com>
2024-01-22 15:06:01 +0530

    data modeling setup

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,24 +2,39 @@ const redis = require('redis');
 const redisClient = redis.createClient();
 
 const Post = require('../models/post');
+const User = require('../models/user');
+const Comment = require('../models/comment');
 
 redisClient.on('error', (err) => {
     console.log('Redis error: ', err);
 });
 
 exports.createPost = async (req, res) => {
-    const post = new Post(req.body);
+    const { title, content, userId } = req.body;
+
     try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const post = new Post({
+            title,
+            content,
+            author: user._id,
+        });
+
         await post.save();
-        res.status(201).send(post);
-    } catch (err) {
-        res.status(400).send(err);
+
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 };
 
 exports.getPosts = async (req, res) => {
     try {
-        const posts = await Post.find({});
+        const posts = await Post.find({}).populate('author');
         res.send(posts);
     } catch (err) {
         res.status(500).send(err);
@@ -39,7 +54,7 @@ exports.getPost = async (req, res) => {
         } else {
             // If the post is not in Redis, get it from the database
             try {
-                const post = await Post.findById(id);
+                const post = await Post.findById(id).populate('author').populate('comments');
                 if (!post) {
                     return res.status(404).send();
                 }
